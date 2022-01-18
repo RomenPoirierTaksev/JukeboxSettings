4d0eac97b004c759e6349f9a53bcda3338ce7d7e
Romen Poirier Taksev <romenpt@hotmail.com>
2022-01-18 16:29:06 -0500

    initial commit

---
+++ src/main/java/RomenPT/JukeboxSettings/JukeboxSettingsApplication.java
@@ -0,0 +1,13 @@
+package RomenPT.JukeboxSettings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JukeboxSettingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JukeboxSettingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RomenPT/JukeboxSettings/JukeboxSettingsApplicationTests.java
@@ -0,0 +1,13 @@
+package RomenPT.JukeboxSettings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JukeboxSettingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
